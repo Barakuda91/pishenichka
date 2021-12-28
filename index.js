b61893939aa461aa7d6b3b1e44e1d0dd5ba0f34a
Aleksandr Istomin <barakudatm@gmail.com>
2021-12-29 00:31:11 +0200

    WIP

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const Wheat = require('./lib/Wheat');
 const Barley = require('./lib/Barley');
 const Weather = require('./lib/Weather');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const { randomIntFromInterval } = require('./lib/tools');
 
 const port = 1995;
 const DAYS = 365;
-const DAY_DURATION = 400;
+const DAY_DURATION = 200;
 
 let currentTime = Date.now();
 let currentDay = 0;
@@ -24,25 +25,25 @@ let weather = new Weather();
 
 // ИМПРОВИЗИРОВАНАЯ БАЗА
 const entities = {
-    wealds: [ // доступные поля для аренды/покупки
-        { size: 19, isFree: true, rentPrice: 1805, salePrice: 13262, owner: null },
-        { size: 32, isFree: true, rentPrice: 3040, salePrice: 22336, owner: null },
-        { size: 59, isFree: true, rentPrice: 5605, salePrice: 41182, owner: null },
-        { size: 78, isFree: true, rentPrice: 7410, salePrice: 54444, owner: null }
+    fields: [ // доступные поля для аренды/покупки
+        { size: 19, isFree: true, rentPrice: 1805, salePrice: 13262, owner: null, status: null },
+        { size: 32, isFree: true, rentPrice: 3040, salePrice: 22336, owner: null, status: null },
+        { size: 59, isFree: true, rentPrice: 5605, salePrice: 41182, owner: null, status: null },
+        { size: 78, isFree: true, rentPrice: 7410, salePrice: 54444, owner: null, status: null }
     ],
+    seeds: [
+        { name: 'Пишеничка', type: 'wheat', salePrice: 150 },
+        { name: 'Ячменё', type: 'barley', salePrice: 190 }
+    ]
 };
 
 const user = {
+    _id: '7823-1231-412321-1231-3123',
     crops: {}, // посевы - тут идентификатор поля, статус
-    warehouse: {}, // склады
+    warehouse: { seeds: { wheat: 0, barley: 0 }, harvest: { wheat: 0, barley: 0 }}, // склады
     balance: 10000
 };
 // КОНЕЦ БАЗЕ
-
-const plants = [
-    new Wheat(),
-    new Barley()
-];
 
 const action = async () => {
 
@@ -50,13 +51,13 @@ const action = async () => {
         currentDay++;
         const temp = weather.getDayTemp(currentDay);
 
-        console.log(`It is ${currentDay} day of ${currentYear} year. Temp ${temp} C`);
+        // console.log(`It is ${currentDay} day of ${currentYear} year. Temp ${temp} C`);
 
-        for (const plantId in plants) {
-            plants[plantId].newDay(temp);
-
-            if (plants[plantId].isIDie())
-                plants.splice(plantId, 1);
+        for (const id in entities.fields) {
+            if (entities.fields[id].crop) {
+                const entity = entities.fields[id].crop
+                entity.newDay(temp);
+            }
         }
 
         if (currentDay >= DAYS) {
@@ -69,14 +70,129 @@ const action = async () => {
     }
 
 };
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('views/public'));
 app.get('/', (req, res) => {
     res.sendFile('views/index.html', { root: __dirname });
 });
 
 app.post('/get_update', (req, res) => {
     res.json({
-        plants, currentDay, currentYear, temp: weather.getDayTemp(currentDay), balance: 10000
+        currentDay, currentYear,
+        temp: weather.getDayTemp(currentDay),
+        balance: user.balance,
+        entities,
+        user
+    });
+});
+
+app.post('/field/sow', (req, res) => {
+    const field = entities.fields[req.body.id];
+    if (field.owner !== user._id)
+        return res.json({ error: 'Это не ваше поле' });
+
+    const seed = entities.seeds.find((el) => el.type === req.body.seedType);
+
+    if (!seed)
+        return res.json({ error: 'Не правильно указан тип семян' });
+
+
+    if (user.warehouse.seeds[seed.type] < field.size)
+        return res.json({ error: 'Не хватает семян' });
+
+    entities.fields[req.body.id].crop = new Wheat();
+    user.warehouse.seeds[seed.type] -= field.size;
+});
+
+app.post('/field/harvest', (req, res) => {
+    const field = entities.fields[req.body.id];
+
+    if (field.owner !== user._id)
+        return res.json({ error: 'Это не ваше поле' });
+
+    if (!field.crop)
+        return res.json({ error: 'Это поле не засеяно' });
+
+    if (field.crop.period !== 'AGING')
+        return res.json({ error: 'Урожай не созрел' });
+
+    user.warehouse.harvest[field.crop.seedType] += field.crop.currentHarvest * field.size;
+    field.crop = null;
+});
+
+app.post('/field/sell', (req, res) => { return res.json({ error: 'Не имплементированно' }); });
+
+app.post('/field/clear', (req, res) => {
+    const field = entities.fields[req.body.id];
+
+    if (field.owner !== user._id)
+        return res.json({ error: 'Это не ваше поле' });
+
+    if (field.crop.period === 'DIE')
+        field.crop = null;
+
+    res.json({ code: 200 });
+});
+
+app.post('/field/buy', (req, res) => {return res.json({ error: 'Не имплементированно' });});
+
+app.post('/harvest/sell', (req, res) => {
+    if (user.warehouse.harvest[req.body.harvestType] <= 0)
+        return res.json({ error: 'Нечего продавать' });
+
+    user.balance += user.warehouse.harvest[req.body.harvestType] / 5;
+    user.warehouse.harvest[req.body.harvestType] = 0;
+
+    res.json({ code: 200 });
+});
+
+app.post('/seeds/buy', (req, res) => {
+    const seed = entities.seeds.find((el) => el.type === req.body.seedType);
+    req.body.quantity = Number(req.body.quantity);
+    if (!seed)
+        return res.json({ error: 'Не правильно указан тип семян' });
+
+    if (seed.salePrice * req.body.quantity > user.balance)
+        return res.json({ error: 'У вас не достаточно денег' });
+
+    user.balance -= seed.salePrice * req.body.quantity;
+    user.warehouse.seeds[req.body.seedType] += req.body.quantity;
+
+    res.json({ code: 200 });
+});
+
+app.post('/field/unrent', (req, res) => {
+    const field = entities.fields[req.body.id];
+
+    if (field.owner !== user._id)
+        return res.json({ error: 'Это не ваше поле' });
+
+    field.owner = null;
+    field.status = null;
+
+    res.json({ code: 200 });
+});
+
+app.post('/field/rent', (req, res) => {
+    const field = entities.fields[req.body.id];
+
+    if (field.owner)
+        return res.json({ error: 'Это поле занято' });
+    if (field.rentPrice > user.balance)
+        return res.json({ error: 'У вас не достаточно денег' });
+
+    user.balance -= field.rentPrice;
+    field.owner = user._id;
+    field.status = 'rent';
+
+    res.json({ code: 200 });
+});
+
+app.post('/field/buy', (req, res) => {
+
+    res.json({
+        plants, currentDay, currentYear, temp: weather.getDayTemp(currentDay), balance: 10000, entities
     });
 });
 
